fb28d1a0d8e78108b924f27abbb6a39870cbd573
FabianVarela <jfabian.303@gmail.com>
2024-07-31 15:58:28 -0500

    Create package to set service layer

---
+++ packages/pagination_service/lib/src/pagination_service.dart
@@ -0,0 +1,7 @@
+/// {@template pagination_service}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class PaginationService {
+  /// {@macro pagination_service}
+  const PaginationService();
+}
---
+++ packages/pagination_service/test/src/pagination_service_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pagination_service/pagination_service.dart';
+
+void main() {
+  group('PaginationService', () {
+    test('can be instantiated', () {
+      expect(PaginationService(), isNotNull);
+    });
+  });
+}
