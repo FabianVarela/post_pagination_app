e8076f97c006f016c79a6978ae59b1f7cd473765
FabianVarela <jfabian.303@gmail.com>
2025-02-17 17:36:26 -0500

    Change import paths

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:post_pagination_app/l10n/l10n.dart';
+import 'package:post_pagination_app/l10n/generated/app_localizations.dart'
+    show AppLocalizations;
 import 'package:post_pagination_app/posts/posts.dart';
 
 class App extends StatelessWidget {
--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:post_pagination_app/l10n/l10n.dart';
+import 'package:post_pagination_app/l10n/generated/app_localizations.dart'
+    show AppLocalizations;
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
