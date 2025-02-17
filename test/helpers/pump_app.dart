558b61c4890e0aed800136d95908bb1d09bbe0b8
FabianVarela <jfabian.303@gmail.com>
2025-02-17 18:46:20 -0500

    Modify import and export paths in l10 file

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:post_pagination_app/l10n/generated/app_localizations.dart'
-    show AppLocalizations;
+import 'package:post_pagination_app/l10n/l10n.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
