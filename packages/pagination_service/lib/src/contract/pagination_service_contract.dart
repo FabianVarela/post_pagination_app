efbf1ad50e6baa59b9ec44715fb321c5238541c9
FabianVarela <jfabian.303@gmail.com>
2025-06-21 23:38:50 -0500

    Add methods to get albums and photos

--- packages/pagination_service/lib/src/contract/pagination_service_contract.dart
+++ packages/pagination_service/lib/src/contract/pagination_service_contract.dart
@@ -7,4 +7,17 @@ abstract interface class PaginationServiceContract {
     required int page,
     required int limit,
   });
+
+  /// [getAlbumList] method used to search a list of albums
+  Future<List<AlbumResponseModel>> getAlbumList({
+    required int page,
+    required int limit,
+  });
+
+  /// [getPhotoList] method used to search a list of photos by [albumId]
+  Future<List<PhotoResponseModel>> getPhotoList({
+    required int albumId,
+    required int page,
+    required int limit,
+  });
 }
--- packages/pagination_service/lib/src/pagination_service.dart
+++ packages/pagination_service/lib/src/pagination_service.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:pagination_service/src/contract/pagination_service_contract.dart';
+import 'package:pagination_service/src/models/albums/album_response_model.dart';
+import 'package:pagination_service/src/models/photos/photo_response_model.dart';
 import 'package:pagination_service/src/models/posts/post_response_model.dart';
 
 /// {@template pagination_service}
-/// Service used to fetch post data from Json Placeholder API
+/// Service used to fetch data from Json Placeholder API
 /// {@endtemplate}
 class PaginationService implements PaginationServiceContract {
   /// {@macro pagination_service}
@@ -13,6 +15,8 @@ class PaginationService implements PaginationServiceContract {
   final Dio dio;
 
   static const _postPath = 'posts';
+  static const _albumPath = 'albums';
+  static const _photosPath = 'photos';
 
   @override
   Future<List<PostResponseModel>> getPostList({
@@ -32,6 +36,53 @@ class PaginationService implements PaginationServiceContract {
       ];
     } else {
       throw Exception('Error getting posts');
+    }
+  }
+
+  @override
+  Future<List<AlbumResponseModel>> getAlbumList({
+    required int page,
+    required int limit,
+  }) async {
+    final response = await dio.get<dynamic>(
+      _albumPath,
+      queryParameters: <String, dynamic>{'_page': page, '_limit': limit},
+    );
+
+    if (response.statusCode == 200) {
+      final responseList = response.data as List<dynamic>;
+      return [
+        for (final item in responseList)
+          AlbumResponseModel.fromJson(item as Map<String, dynamic>),
+      ];
+    } else {
+      throw Exception('Error getting albums');
+    }
+  }
+
+  @override
+  Future<List<PhotoResponseModel>> getPhotoList({
+    required int albumId,
+    required int page,
+    required int limit,
+  }) async {
+    final response = await dio.get<dynamic>(
+      _photosPath,
+      queryParameters: <String, dynamic>{
+        'albumId': albumId,
+        '_page': page,
+        '_limit': limit,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final responseList = response.data as List<dynamic>;
+      return [
+        for (final item in responseList)
+          PhotoResponseModel.fromJson(item as Map<String, dynamic>),
+      ];
+    } else {
+      throw Exception('Error getting photos');
     }
   }
 }
