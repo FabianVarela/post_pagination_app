fa20c2a79ecc093f365e6977d644c5101468d341
FabianVarela <jfabian.303@gmail.com>
2025-06-21 23:42:28 -0500

    Remove test method

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:post_pagination_app/app/app.dart';
-import 'package:post_pagination_app/posts/posts.dart';
-
-void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(PostsPage), findsOneWidget);
-    });
-  });
-}
+void main() {}
