61714884831365018aa854ce188aaeed539928f9
FabianVarela <jfabian.303@gmail.com>
2026-04-04 23:39:11 -0500

    Set values in headers to allow requests

--- lib/app/dependencies/app_register.dart
+++ lib/app/dependencies/app_register.dart
@@ -12,6 +12,13 @@ class AppRegister {
           BaseOptions(
             // ignore: avoid_redundant_argument_values
             baseUrl: const String.fromEnvironment('BASE_API_URL'),
+            headers: <String, dynamic>{
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/120.0.0.0 Safari/537.36',
+              'Accept': 'application/json',
+              'Accept-Language': 'en-US,en;q=0.9',
+            },
           ),
         ),
       ),
